df85dc730abff66b1888af3cdd977f1c90a2c782
Rafael Miranda <58390974+Rafa84@users.noreply.github.com>
2024-06-11 22:00:06 -0300

    Add files via upload
    Uso do input

---
+++ Aula 02.py
@@ -0,0 +1,5 @@
+#Exercicio 2
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input('Me diga seu nome: ')
+print("Seja muito bem vindo(a) {}".format(nome))
